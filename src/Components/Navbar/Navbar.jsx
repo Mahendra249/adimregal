65a9bc7ba67c30607a2c2a8594b3b4213f2eeb37
DSRCODE <daljeetkalyanwat62@gmail.com>
2023-12-30 09:50:09 +0530

    Initial commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Navbar.css';
+
+// Image
+import profile from '../../assets/adam.jfif'
+
+const Navbar = () => {
+  return (
+    <div className='navigation'>
+        <div className="logo">
+            <h1>RegalTrendZ</h1>
+        </div>
+        <div className="admin_profile">
+            <div className="avatar">
+                <img src={profile} alt="" />
+            </div>
+            <div className="avatar_details">
+                <p>Adam Adkins</p>
+                <p>adam@gmail.com</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
